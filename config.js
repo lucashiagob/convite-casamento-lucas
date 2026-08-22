5756d810a49bbc90f6a16a7bfe5c4de978970bd1
lucashiagob <54961374+lucashiagob@users.noreply.github.com>
2026-08-22 19:57:28 -0300

    Add wedding configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+window.WEDDING_CONFIG = {
+  couple: "Lucas & Shayane",
+  date: "17 de abril de 2027",
+  time: "15h",
+  venue: "Sítio Varrelinha",
+  address: "Sítio Varrelinha, Maceió - AL",
+  mapQuery: "Sítio Varrelinha, Maceió - AL",
+  message: "O nosso grande dia está chegando, e será uma alegria celebrar esse momento com você.",
+  pixKey: "82987125101",
+  pixName: "LUCAS E SHAYANE",
+  pixCity: "MACEIO",
+  supabaseUrl: "https://eaovmjvqbfsztowwlnbb.supabase.co",
+  supabasePublishableKey: "sb_publishable_thY9QO7y6FLOIa2x9ACiHg_WtaewdNv"
+};
